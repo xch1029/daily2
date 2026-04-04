fac9b3236fde9fd6fc52361c8e23de64575f0501
xxq0411 <416876921@qq.com>
2026-04-04 17:27:31 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite-plus";
+import vue from "@vitejs/plugin-vue";
+
+// https://vite.dev/config/
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [vue()],
+});
